ae51bee7f29da4f4a816846e0af136bbe0a83168
abdullahparvin <abdullahparvin_@hotmail.com>
2024-05-21 16:26:29 -0500

    Fixed module error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Editor from "@monaco-editor/react";
 import Navbar from './Components/Navbar';
 import Axios from 'axios';
-import spinner from './spinner.svg';
+import spinner from './Spinner.svg';
 
 function App() {
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
-import '../Styles/Navbar.css';
+import './Navbar.css';
 
 const Navbar = ({ userLang, setUserLang, userTheme,
     setUserTheme, fontSize, setFontSize }) => {
